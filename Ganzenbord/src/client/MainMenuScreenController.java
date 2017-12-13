d4b9c74723a745175186dee73347ac8c43f765f9
GideonvDoorn <gideon.vandoorn@student.fontys.nl>
2017-12-13 20:28:31 +0100

    Basic main menu screen functionality

--- Ganzenbord/src/client/MainMenuScreenController.java
+++ Ganzenbord/src/client/MainMenuScreenController.java
@@ -1,4 +1,37 @@
 package client;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class MainMenuScreenController {
+
+    @FXML
+    public Label lblUsername;
+
+
+    @FXML
+    public void initialize() {
+        //TODO: -Database, loginserver- get current user name, and put it in this label
+        lblUsername.setText("Gideon -test");
+    }
+
+    public void btnLogoutOnClick(){
+        //TODO: -Database, loginserver- logout user
+        UITools.UIManager uiManager = new UITools.UIManager();
+        uiManager.loadFXML("LoginScreen.fxml");
+    }
+
+    public void btnStartGameOnClick(){
+
+
+        UITools.UIManager uiManager = new UITools.UIManager();
+        uiManager.loadFXML("LobbyScreen.fxml");
+    }
+
+    public void btnJoinGameOnClick(){
+
+
+        UITools.UIManager uiManager = new UITools.UIManager();
+        uiManager.loadFXML("LobbyScreen.fxml");
+    }
 }
